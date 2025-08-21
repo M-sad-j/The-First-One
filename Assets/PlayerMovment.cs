2e625446ed3cba499480f75820f2436ee094d818
Modie <jarirehms81@gmail.com>
2025-08-22 01:47:52 +0330

    Camera Follows Us

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -6,14 +6,6 @@ public class PlayerMovment : MonoBehaviour
     
     public float forwardForce = 2000f;
     public float SidewaysForce = 500f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    //?void Start()
-    //{
-    //Debug.Log("Hello, Wolrd!");
-    //Rb.AddForce(0, 200, 500);
-    //}
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         Rb.AddForce(0, 0, forwardForce * Time.deltaTime);
