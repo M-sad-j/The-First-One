78ea15fc6fed3eaea495cf3d8a61132206073a7e
Modie <jarirehms81@gmail.com>
2025-08-22 12:26:33 +0330

    Obstacle, singular

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter (Collision collisioninfo)
+    {
+        if (collisioninfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+        
+    }
+}
