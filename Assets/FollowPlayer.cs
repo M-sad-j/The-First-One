ccf7f216c808b0dcdd77a0e2961e580f5b7168c9
Modie <jarirehms81@gmail.com>
2025-08-22 11:47:31 +0330

    Update FollowPlayer.cs

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -5,7 +5,7 @@ public class FollowPlayer : MonoBehaviour
     public Transform player;
     public Vector3 offset;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         transform.position = player.position + offset;
     }
